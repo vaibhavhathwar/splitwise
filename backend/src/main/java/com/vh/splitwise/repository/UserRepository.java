4b12114868a1f51cb80e856926672b59c81959fe
vaibhavhathwar <vaibhavhathwar404@gmail.com>
2025-05-29 22:42:47 +0530

    Started with spring security for login and signup

---
+++ backend/src/main/java/com/vh/splitwise/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.vh.splitwise.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.vh.splitwise.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+  Optional<User> findByEmail(String email);
+
+  Optional<User> findByUsername(String username);
+}
---
+++ backend/src/main/java/com/vh/splitwise/service/UserService.java
@@ -0,0 +1,42 @@
+package com.vh.splitwise.service;
+
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.vh.splitwise.entity.User;
+import com.vh.splitwise.repository.UserRepository;
+
+@Service
+public class UserService {
+
+  private PasswordEncoder passwordEncoder;
+  private UserRepository userRepository;
+
+  public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    this.passwordEncoder = passwordEncoder;
+    this.userRepository = userRepository;
+  }
+
+  private boolean signUp(User user) {
+    String password = user.getPassword();
+    String encodedPassword = passwordEncoder.encode(password);
+    user.setPassword(encodedPassword);
+    userRepository.save(user);
+  }
+
+  private boolean logIn(String email, String password) {
+    User user = null;
+    Optional<User> optionalUser = userRepository.findByEmail(email);
+    if (optionalUser.isPresent())
+      user = optionalUser.get();
+    else
+      return false;
+    String encodedPasswrod = passwordEncoder.encode(password);
+    if (passwordEncoder.matches(password, encodedPasswrod))
+      return true;
+    else
+      return false;
+  }
+}
